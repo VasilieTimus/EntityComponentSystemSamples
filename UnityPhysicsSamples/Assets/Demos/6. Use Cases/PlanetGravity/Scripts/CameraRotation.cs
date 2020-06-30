32a9ee41741199045952c140da3f71ae0541f9f1
Vasile Timus <31159802+NinjaNH99@users.noreply.github.com>
2020-06-30 16:57:12 +0300

    Update

--- UnityPhysicsSamples/Assets/Demos/6. Use Cases/PlanetGravity/Scripts/CameraRotation.cs
+++ UnityPhysicsSamples/Assets/Demos/6. Use Cases/PlanetGravity/Scripts/CameraRotation.cs
@@ -7,8 +7,8 @@ namespace Demos._6._Use_Cases.PlanetGravity.Scripts
 	{
 		private void Start()
 		{
-			transform.DORotate(Vector3.up * 35f, 5f, RotateMode.FastBeyond360).SetEase(Ease.Linear).SetLoops(-1, LoopType.Incremental);
-			transform.DOScale(Vector3.one * 1.5f, 5f).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+			transform.DORotate(Vector3.up * 35f, 4f, RotateMode.FastBeyond360).SetEase(Ease.Linear).SetLoops(-1, LoopType.Incremental);
+			transform.DOScale(Vector3.one * 1.15f, 4f).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
 		}
 	}
 }
--- UnityPhysicsSamples/Assets/Demos/6. Use Cases/PlanetGravity/Scripts/PlanetGravityAuthoring.cs
+++ UnityPhysicsSamples/Assets/Demos/6. Use Cases/PlanetGravity/Scripts/PlanetGravityAuthoring.cs
@@ -8,73 +8,101 @@ using Random = Unity.Mathematics.Random;
 
 public struct PlanetGravity : IComponentData
 {
-    public float3 GravitationalCenter;
-    public float GravitationalMass;
-    public float GravitationalConstant;
-    public float EventHorizonDistance;
-    public float RotationMultiplier;
+	public float3 GravitationalCenter;
+	public float GravitationalMass;
+	public float GravitationalConstant;
+	public float EventHorizonDistance;
+	public float RotationMultiplier;
+
+	public float timer;
+	public bool canUpdate;
 }
 
 public class PlanetGravityAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 {
-    public float GravitationalMass;
-    public float GravitationalConstant;
-    public float EventHorizonDistance;
-    public float RotationMultiplier;
+	public float GravitationalMass;
+	public float GravitationalConstant;
+	public float EventHorizonDistance;
+	public float RotationMultiplier;
 
-    void IConvertGameObjectToEntity.Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
-    {
-        var component = new PlanetGravity
-        {
-            GravitationalCenter = transform.position,
-            GravitationalMass = GravitationalMass,
-            GravitationalConstant = GravitationalConstant,
-            EventHorizonDistance = EventHorizonDistance,
-            RotationMultiplier = RotationMultiplier
-        };
-        dstManager.AddComponentData(entity, component);
+	void IConvertGameObjectToEntity.Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+	{
+		var component = new PlanetGravity
+		{
+			GravitationalCenter = transform.position,
+			GravitationalMass = GravitationalMass,
+			GravitationalConstant = GravitationalConstant,
+			EventHorizonDistance = EventHorizonDistance,
+			RotationMultiplier = RotationMultiplier
+		};
+		dstManager.AddComponentData(entity, component);
 
-        if (dstManager.HasComponent<PhysicsMass>(entity))
-        {
-            var bodyMass = dstManager.GetComponentData<PhysicsMass>(entity);
-            var random = new Random();
-            random.InitState(10);
-            bodyMass.InverseMass = random.NextFloat(bodyMass.InverseMass, bodyMass.InverseMass * 4f);
+		if (dstManager.HasComponent<PhysicsMass>(entity))
+		{
+			var bodyMass = dstManager.GetComponentData<PhysicsMass>(entity);
+			var random = new Random();
+			random.InitState(10);
+			bodyMass.InverseMass = random.NextFloat(bodyMass.InverseMass, bodyMass.InverseMass * 4f);
 
-            dstManager.SetComponentData(entity, bodyMass);
-        }
-    }
+			dstManager.SetComponentData(entity, bodyMass);
+		}
+	}
 }
 
 [UpdateBefore(typeof(BuildPhysicsWorld))]
 public class PlanetGravitySystem : SystemBase
 {
-    static readonly quaternion k_GravityOrientation = quaternion.RotateY(math.PI / 2f);
+	static readonly quaternion k_GravityOrientation = quaternion.RotateY(math.PI / 2f);
 
-    protected override void OnUpdate()
-    {
-        var dt = UnityEngine.Time.fixedDeltaTime;
+	protected override void OnUpdate()
+	{
+		var dt = UnityEngine.Time.fixedDeltaTime;
+		var dtSimple = UnityEngine.Time.deltaTime;
 
-        Entities
-            .WithName("ApplyGravityFromPlanet")
-            .WithBurst()
-            .ForEach((ref PhysicsMass bodyMass, ref PhysicsVelocity bodyVelocity, in Translation pos, in PlanetGravity gravity) =>
-        {
-            float mass = math.rcp(bodyMass.InverseMass);
+		Entities
+			.WithName("ApplyGravityFromPlanet")
+			.WithBurst()
+			.ForEach((ref PhysicsMass bodyMass, ref PhysicsVelocity bodyVelocity, ref PlanetGravity gravity, in Translation pos) =>
+			{
+				float mass = math.rcp(bodyMass.InverseMass);
 
-            float3 dir = (gravity.GravitationalCenter - pos.Value);
-            float dist = math.length(dir);
-            float invDist = 1.0f / dist;
-            dir = math.normalize(dir);
-            float3 xtraGravity = (gravity.GravitationalConstant * (gravity.GravitationalMass * mass) * dir) * invDist * invDist;
-            bodyVelocity.Linear += xtraGravity * dt;
-            
-            if (dist < gravity.EventHorizonDistance)
-            {
-                xtraGravity = (gravity.RotationMultiplier * gravity.GravitationalConstant * gravity.GravitationalMass * dir) * invDist;
-                bodyVelocity.Linear += math.rotate(k_GravityOrientation, xtraGravity) * gravity.RotationMultiplier * dt;
-            }
-            
-        }).Schedule();
-    }
+				float3 dir = (gravity.GravitationalCenter - pos.Value);
+				float dist = math.length(dir);
+				float invDist = 1.0f / dist;
+				dir = math.normalize(dir);
+				
+				float3 xtraGravity = (gravity.RotationMultiplier * gravity.GravitationalConstant * gravity.GravitationalMass * dir) * invDist;
+
+				if (dist < gravity.EventHorizonDistance)
+				{
+					if (gravity.canUpdate)
+					{
+						bodyVelocity.Linear += math.rotate(k_GravityOrientation, xtraGravity) * gravity.RotationMultiplier * dt;
+
+						gravity.timer -= dtSimple;
+						
+						if (gravity.timer <= 0)
+						{
+							gravity.canUpdate = false;
+						}
+					}
+					else
+					{
+						gravity.timer += dtSimple;
+						xtraGravity = (gravity.GravitationalConstant * (gravity.GravitationalMass * mass) * dir) * invDist * invDist;
+						bodyVelocity.Linear += xtraGravity * dt;
+
+						if (gravity.timer >= 2f)
+						{
+							gravity.canUpdate = true;
+						}
+					}
+				}
+				else
+				{
+					bodyVelocity.Linear += xtraGravity * dt;	
+				}
+
+			}).Schedule();
+	}
 }
